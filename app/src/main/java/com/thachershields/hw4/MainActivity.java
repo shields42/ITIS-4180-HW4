6007b22784e2b53f74f2caa4173d043d60243ece
Thacher Shields <thacher@mac.com>
2017-10-11 15:00:20 -0400

    Added GetSimpleAsync

--- app/src/main/java/com/thachershields/hw4/MainActivity.java
+++ app/src/main/java/com/thachershields/hw4/MainActivity.java
@@ -1,11 +1,23 @@
 package com.thachershields.hw4;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
